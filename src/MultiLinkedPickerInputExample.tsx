e3bea65857e460e6f0ab0f549b878b6279984b81
Dmytro Derhachov <dergachovda@gmail.com>
2023-08-04 12:11:31 +0300

    GPT refactoring

--- src/MultiLinkedPickerInputExample.tsx
+++ src/MultiLinkedPickerInputExample.tsx
@@ -1,76 +1,79 @@
-import React, { useState } from 'react';
-import { FlexRow, PickerInput, FlexCell } from '@epam/promo';
+import React, { useCallback, useState } from 'react';
+// @ts-ignore
+import { ReactComponent as DelIcon } from '@epam/assets/icons/common/action-delete-18.svg';
+// @ts-ignore
+import { ReactComponent as AddIcon } from '@epam/assets/icons/common/action-add-18.svg';
+import { Button, FlexRow, PickerInput, FlexCell } from '@epam/promo';
 import { useArrayDataSource } from '@epam/uui-core';
-import { languageLevels } from './Data'
+import { languageLevels } from './Data';
 
 const fullLevelsList = languageLevels;
 
 export default function LanguagesMultiPicker() {
-    const [pickerValueMap, setPickerValueMap] = useState(
-        new Map<string, number>()
+    const [pickerValueMap, setPickerValueMap] = useState(new Map<string, number>());
+
+    const FilteredPickerInput = useCallback(
+        ({ pickerName }) => {
+            const [currentPickerValue, setCurrentPickerValue] = useState(
+                pickerValueMap.get(pickerName)
+            );
+
+            const filteredLanguageLevelsDataSource = useArrayDataSource(
+                {
+                    items: fullLevelsList.filter((item) => {
+                        return !pickerValueMap.has(pickerName) || pickerValueMap.get(pickerName) === item.id;
+                    }),
+                },
+                [pickerValueMap]
+            );
+
+            return (
+                <PickerInput
+                    dataSource={filteredLanguageLevelsDataSource}
+                    value={currentPickerValue}
+                    onValueChange={(value: number) => {
+                        setCurrentPickerValue(value);
+                        setPickerValueMap(new Map([...pickerValueMap.entries(), [pickerName, value]]));
+                    }}
+                    getName={(item) => item.level}
+                    entityName="Language level"
+                    selectionMode="single"
+                    valueType="id"
+                    sorting={{ field: 'level', direction: 'asc' }}
+                />
+            );
+        },
+        [pickerValueMap]
     );
 
-    function FilteredPickerInput(props : any) {
-        const { pickerName, pickerValueMap } = props;
-        const [currentPickerValue, setCurrentPickerValue] = useState(pickerValueMap.get(pickerName));
-        const filteredLanguageLevelsDataSource = useArrayDataSource(
-            {
-                items: fullLevelsList.filter(
-                        (item) => {
-                            const selected = new Set(pickerValueMap.values());
-                            // console.log("currentPickerValue:", currentPickerValue, "item.id", item.id, !selected.has(item.id), item.id === currentPickerValue);
-                            const b = !selected.has(item.id) || item.id === currentPickerValue;
-                            return b;
-                        }
-                    )
-            },
-            [pickerValueMap]
-        );
+    const addNewRow = useCallback(() => {
+        setPickerValueMap(new Map([...pickerValueMap, [`picker${pickerValueMap.size}`, undefined]]));
+    }, [pickerValueMap]);
 
-        console.log("-pickerName: ", pickerName);
-        console.log(" pickerValue:", pickerValueMap.get(pickerName));
-        console.log(" currentPickerValue:", currentPickerValue);
-        console.log(" Level:", fullLevelsList.find( (item) => currentPickerValue === item.id)?.level);
-        console.log(" filteredLanguageLevelsDataSource:", filteredLanguageLevelsDataSource.props);
-        console.log("-----");
+    const removeRow = useCallback(() => {
+        const mapCopy = new Map(pickerValueMap);
+        mapCopy.delete([...mapCopy.keys()].pop());
+        setPickerValueMap(mapCopy);
+    }, [pickerValueMap]);
 
-        return (
-            <PickerInput
-                dataSource={filteredLanguageLevelsDataSource}
-                value={currentPickerValue}
-                onValueChange={(value: number) => {
-                    // setPickerValueMap(pickerValueMap.set(pickerName, value));
-                    setCurrentPickerValue(value)
-                    setPickerValueMap(new Map([...pickerValueMap.entries(), [pickerName, value]]));
-                }}
-                getName={ (item) => item.level }
-                entityName="Language level"
-                selectionMode="single"
-                valueType="id"
-                sorting={ { field: 'level', direction: 'asc' } }
-            />
-        );
-    }
-    const pickerCount = 3;
-    const pickerArray = Array.from(Array(pickerCount), (_, i) => {
-        const pickerName = `picker${i}`;
-        // console.log("pickerName: ", pickerName, "pickerValue:", pickerValueMap.get(pickerName))
-        return (
+    return (
+        <FlexCell width={612}>
+            {[...pickerValueMap.keys()].map((pickerName) => (
+                <FlexRow key={pickerName} spacing="12">
+                    <FilteredPickerInput pickerName={pickerName} />
+                </FlexRow>
+            ))}
             <FlexRow spacing="12">
-                <FilteredPickerInput
-                    key={pickerName}
-                    pickerName={pickerName}
-                    pickerValueMap={pickerValueMap}
-                    setPickerValueMap={setPickerValueMap}
+                <Button icon={AddIcon} fill="light" color="blue" caption="Add row" onClick={addNewRow} />
+                <Button
+                    icon={DelIcon}
+                    fill="light"
+                    color="blue"
+                    caption="Remove row"
+                    isDisabled={pickerValueMap.size <= 1}
+                    onClick={removeRow}
                 />
             </FlexRow>
-        );
-    });
-
-    // @ts-ignore
-    return (
-        <FlexCell width={ 612 }>
-            { pickerArray }
         </FlexCell>
     );
 }
